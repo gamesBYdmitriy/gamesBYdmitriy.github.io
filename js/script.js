0333398d93249698ae59729ffdf269918bc9374b
gamesBYdmitriy <109818683+gamesBYdmitriy@users.noreply.github.com>
2022-09-17 14:15:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Привет");
+var b1 = document.querySelector(".b1");
+var b2 = document.querySelector(".b2");
+var b3 = document.querySelector(".b3");
+b1.addEventListener("click", function(){
+    window.location.href = './Game1/game1.html';
+});
+b2.addEventListener("click", function(){
+    window.location.href = './Game2/game2.html';
+});
+b3.addEventListener("click", function(){
+    window.location.href = './Game3/game3.html';
+});
