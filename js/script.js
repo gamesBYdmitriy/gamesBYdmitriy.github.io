c48d9a1ecb89b2408e807aac70dc4551c5efac25
gamesBYdmitriy <109818683+gamesBYdmitriy@users.noreply.github.com>
2022-10-20 18:29:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var b1 = document.querySelector(".b1");
 var b2 = document.querySelector(".b2");
 var b3 = document.querySelector(".b3");
+var b4 = document.querySelector(".b4");
 b1.addEventListener("click", function(){
     window.location.href = './Game1/game1.html';
 });
@@ -9,4 +10,7 @@ b2.addEventListener("click", function(){
 });
 b3.addEventListener("click", function(){
     window.location.href = './Game3/game3.html';
+});
+b4.addEventListener("click", function(){
+    window.location.href = './game4/index.html';
 });
